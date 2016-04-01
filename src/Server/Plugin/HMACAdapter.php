b9f19c21e76111f4a39c0c2a35e2d2f079126e68
Reinaldo Borges <reinaldoborges@gmail.com>
2016-04-01 18:35:16 -0300

    [ADD] Helper para retornar HMACKeyID ou HMACAdapter a partir do evento (MvcEvent ou ResourceEvent). [ADD] ControllerPlugin utiliza o Helper.

--- src/Server/Plugin/HMACAdapter.php
+++ src/Server/Plugin/HMACAdapter.php
@@ -2,24 +2,18 @@
 
 namespace RB\Sphinx\Hmac\Zend\Server\Plugin;
 
+use RB\Sphinx\Hmac\Zend\Server\HMACServerHelper;
+
 use Zend\Mvc\Controller\Plugin\AbstractPlugin;
 use Zend\Mvc\Controller\AbstractController;
 
 class HMACAdapter extends AbstractPlugin {
 	/**
 	 * Retorna o Adapter HMAC utilizado no HMAC
-	 *
-	 * @param string $param        	
-	 * @param mixed $default        	
-	 * @return mixed
 	 */
 	public function __invoke() {
-		$controller = $this->getController ();
-		if ($controller instanceof AbstractController) {
-			$adapter = $controller->getEvent ()->getParam ( 'RBSphinxHmacAdapter', NULL );
-			if ($adapter !== NULL)
-				return $adapter;
-		}
+		if ($controller instanceof AbstractController)
+			return HMACServerHelper::getHmacAdapter($this->getController()->getEvent());
 		
 		return NULL;
 	}
--- src/Server/Plugin/HMACKeyId.php
+++ src/Server/Plugin/HMACKeyId.php
@@ -2,24 +2,18 @@
 
 namespace RB\Sphinx\Hmac\Zend\Server\Plugin;
 
+use RB\Sphinx\Hmac\Zend\Server\HMACServerHelper;
+
 use Zend\Mvc\Controller\Plugin\AbstractPlugin;
 use Zend\Mvc\Controller\AbstractController;
 
 class HMACKeyId extends AbstractPlugin {
 	/**
 	 * Retorna o KeyID autenticado no HMAC
-	 *
-	 * @param string $param        	
-	 * @param mixed $default        	
-	 * @return mixed
 	 */
 	public function __invoke() {
-		$controller = $this->getController ();
-		if ($controller instanceof AbstractController) {
-			$keyId = $controller->getEvent ()->getParam ( 'RBSphinxHmacAdapterIdentity', NULL );
-			if ($keyId !== NULL)
-				return $keyId ['keyid'];
-		}
+		if ($controller instanceof AbstractController) 
+			return HMACServerHelper::getHmacKeyId($this->getController()->getEvent());
 		
 		return NULL;
 	}
