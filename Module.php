f687e773021285452d377fd634b5a37c98e29d48
andersonlunog <andersonlevita@gmail.com>
2019-11-11 16:20:36 -0300

    Ajustes para apigility zf3

--- Module.php
+++ Module.php
@@ -8,45 +8,44 @@ use Zend\Mvc\MvcEvent;
 use Zend\Mvc\ModuleRouteListener;
 
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
-	public function getAutoloaderConfig() {
-		return array (
-				'Zend\Loader\ClassMapAutoloader' => array (
-						__DIR__ . '/autoload_classmap.php' 
-				),
-				'Zend\Loader\StandardAutoloader' => array (
-						'namespaces' => array (
-								__NAMESPACE__ => __DIR__ . '/src/' 
-						) 
-				) 
-		);
-	}
-	public function getConfig() {
-		return include __DIR__ . '/config/module.config.php';
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public function onBootstrap($e) {
-		$app = $e->getApplication ();
-		$services = $app->getServiceManager ();
-		$em = $app->getEventManager ();
-		
-		/**
-		 * Baixa prioridade, para avaliar necessidade de autenticação HMAC após todas as operações de rota
-		 */
-		$em->attach ( MvcEvent::EVENT_ROUTE, $services->get ( 'RB\Sphinx\Hmac\Zend\Server\HMACListener' ), -1000 );
-		
-		/**
-		 * Baixa prioridade, para acrescentar assinatura HMAC após todas as operações na resposta
-		 */
-		$moduleRouteListener = new ModuleRouteListener();
-		$moduleRouteListener->attach($em);
-		$em -> attach(
-				MvcEvent::EVENT_FINISH,
-				array($services->get ( 'RB\Sphinx\Hmac\Zend\Server\HMACListener' ), 'onFinish'),
-				-1000
-		);
-		
-	}
+  public function getAutoloaderConfig() {
+    return [
+        'Zend\Loader\ClassMapAutoloader' => [
+            __DIR__ . '/autoload_classmap.php' 
+        ],
+        'Zend\Loader\StandardAutoloader' => [
+            'namespaces' => [
+                __NAMESPACE__ => __DIR__ . '/src/' 
+            ] 
+        ] 
+    ];
+  }
+  public function getConfig() {
+    return include __DIR__ . '/config/module.config.php';
+  }
+  
+  /**
+   * {@inheritDoc}
+   */
+  public function onBootstrap($e) {
+    $app = $e->getApplication();
+    $services = $app->getServiceManager();
+    $em = $app->getEventManager();
+    
+    /**
+     * Baixa prioridade, para avaliar necessidade de autenticação HMAC após todas as operações de rota
+     */
+    $em->attach(MvcEvent::EVENT_ROUTE, $services->get('RB\Sphinx\Hmac\Zend\Server\HMACListener'), -1000);
+    
+    /**
+     * Baixa prioridade, para acrescentar assinatura HMAC após todas as operações na resposta
+     */
+    $moduleRouteListener = new ModuleRouteListener();
+    $moduleRouteListener->attach($em);
+    $em->attach(
+        MvcEvent::EVENT_FINISH,
+        array($services->get('RB\Sphinx\Hmac\Zend\Server\HMACListener'), 'onFinish'),
+        -1000
+    );
+  }
 }
